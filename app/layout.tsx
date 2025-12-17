ee7821c713a7b868796078611cd4dce0e75b299a
timiratat-sudo <timiratat@gmail.com>
2025-12-17 18:22:54 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,22 +17,17 @@ export default function RootLayout({
 }) {
   return (
     <html >
-      <script
-        type="text/javascript"
-        dangerouslySetInnerHTML={{
-          __html: `
-          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-          (function(){
-          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-          s1.async=true;
-          s1.src='https://embed.tawk.to/69358ccc66c18919785317ba/1jbsis009';
-          s1.charset='UTF-8';
-          s1.setAttribute('crossorigin','*');
-          s0.parentNode.insertBefore(s1,s0);
-          })();
-          `,
-        }}
-      />
+      <script type="text/javascript">
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      (function(){
+      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+      s1.async=true;
+      s1.src='https://embed.tawk.to/6942e599c73adf1980aa89aa/1jcml1ofo';
+      s1.charset='UTF-8';
+      s1.setAttribute('crossorigin','*');
+      s0.parentNode.insertBefore(s1,s0);
+      })();
+      </script>
       <body className="page-root">
         <AppShell>{children}</AppShell>
       </body>
